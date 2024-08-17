18bea3c94cb3d993ff9addc521f8377c9679d7b4
Saiyedul Abrar <saiyedul.abrar1430@gmail.com>
2024-08-17 22:07:12 +0600

    problem 2.c

--- Lab-1/problem 2.c
+++ Lab-1/problem 2.c
@@ -3,13 +3,13 @@
 int main()
 {
     printf("Kazi Nazrul Islam\n"
-           "\nIndeed, Bengali Muslim’s quest for identity culminated in Kazi Nazrul Islam"
+           "\nIndeed, Bengali Muslimâ€™s quest for identity culminated in Kazi Nazrul Islam"
            "The literary endeavour that the Muslim writers undertook during the 1920s"
            "flourished through Kazi Nazrul Islam in the sense that he was the first Muslim to"
            "proclaim that he was both a Muslim and a Bengali."
            "The two parts of the identity do not conflict; rather they converge at a"
            "point and Nazrul was the first man who got to this point with both his heart and head."
-           "The tragic part of Bengali Muslim’s literary endeavour, historically contextualised,"
+           "The tragic part of Bengali Muslimâ€™s literary endeavor, historically contextualised,"
            "consists in the fact that it revolved around an identity crisis in the early twentieth century."
            "They failed to resolve the dilemma, whether they are Muslims having a Middle East"
            "origin or Bengali native origin. Where do they belong? This very question simply haunted them for years."
