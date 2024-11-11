ebda854b975fcb1ffd223c2c65e28b7d7cb04c0a
Saiyedul Abrar <saiyedul.abrar1430@gmail.com>
2024-11-11 21:00:51 +0600

    Update and rename problem 6.c to sum of two integer without different variable.c

--- Lab-1/sum of two integer without different variable.c
+++ Lab-1/sum of two integer without different variable.c
@@ -1,5 +1,5 @@
 //write a program to add two integer numbers and show
-//the summation keeping in a different variables
+//the summation without keeping in a different variables
 #include<stdio.h>
 int main()
 {
